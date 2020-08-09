1e05afca4a53dfead9f272281cabda60ee1c08f7
Apurva Bagaria <bagaria.apurva@gmail.com>
2020-08-09 12:09:24 +0530

    Logout Added

--- Frontend/src/app/pages/signup-page/signup-page.component.ts
+++ Frontend/src/app/pages/signup-page/signup-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpResponse } from '@angular/common/http';
 import { AuthService } from 'src/app/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup-page',
@@ -9,14 +10,17 @@ import { AuthService } from 'src/app/auth.service';
 })
 export class SignupPageComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   onSignupButtonClicked(email: string, password: string) {
     this.authService.signup(email, password).subscribe((res: HttpResponse<any>) => {
-      console.log(res);
+      if(res.status === 200){
+        this.router.navigate(['/lists']);
+      }
+      // console.log(res);
     })
   }
 
